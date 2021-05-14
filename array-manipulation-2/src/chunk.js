e94123f1c96e9dc40405d25d9a97a12cb0ce9c51
Stefan Carrera <stefancarrera@gmail.com>
2021-05-14 14:27:32 -0500

    Completed array-manipulation-2.

---
+++ array-manipulation-2/src/chunk.js
@@ -0,0 +1,8 @@
+/* exported chunk */
+function chunk(array, size) {
+  var chunks = [];
+  for (var i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+}
