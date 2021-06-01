2f05687e918f7640367096ed73d6564efe8f6b9e
Stefan Carrera <stefancarrera@gmail.com>
2021-06-01 17:24:38 -0500

    Completed javascript-constructors.

---
+++ javascript-constructors/example-constructor.js
@@ -0,0 +1,10 @@
+function ExampleConstructor() {
+
+}
+console.log('value of ExampleConstructor', ExampleConstructor);
+console.log('typeof ExampleConstructor', typeof ExampleConstructor);
+
+var example1 = new ExampleConstructor();
+console.log('value of example1', example1);
+
+console.log('example1 instanceof ExampleConstructor', example1 instanceof ExampleConstructor);
