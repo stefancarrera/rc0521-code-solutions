450ba039ad05ede11f289ee846b9a35a35e22a7a
Stefan Carrera <stefancarrera@gmail.com>
2021-06-02 12:41:19 -0500

    Completed javascript-timers.

---
+++ javascript-timers/interval.js
@@ -0,0 +1,15 @@
+var $h1 = document.querySelector('h1');
+
+function countdown() {
+  if ($h1.textContent === '4') {
+    $h1.textContent = '3';
+  } else if ($h1.textContent === '3') {
+    $h1.textContent = '2';
+  } else if ($h1.textContent === '2') {
+    $h1.textContent = '1';
+  } else if ($h1.textContent === '1') {
+    $h1.textContent = 'Good News Everyone!';
+  }
+}
+
+setInterval(countdown, 1000);
