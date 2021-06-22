5abc3b3840e64c39c10358dae7c56eae8bee4b00
Stefan Carrera <stefancarrera@gmail.com>
2021-06-22 18:15:04 -0500

    Cleaned up the code by adding more variables.

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -2,19 +2,22 @@ const add = require('./add');
 const subtract = require('./subtract');
 const multiply = require('./multiply');
 const divide = require('./divide');
+const operator = process.argv[3];
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[4]);
 
-if (process.argv[3] === 'plus') {
-  console.log('Result:', add(parseInt(process.argv[2]), parseInt(process.argv[4])));
+if (operator === 'plus') {
+  console.log('Result:', add(num1, num2));
 }
 
-if (process.argv[3] === 'minus') {
-  console.log('Result:', subtract(parseInt(process.argv[2]), parseInt(process.argv[4])));
+if (operator === 'minus') {
+  console.log('Result:', subtract(num1, num2));
 }
 
-if (process.argv[3] === 'times') {
-  console.log('Result:', multiply(parseInt(process.argv[2]), parseInt(process.argv[4])));
+if (operator === 'times') {
+  console.log('Result:', multiply(num1, num2));
 }
 
-if (process.argv[3] === 'over') {
-  console.log('Result:', divide(parseInt(process.argv[2]), parseInt(process.argv[4])));
+if (operator === 'over') {
+  console.log('Result:', divide(num1, num2));
 }
