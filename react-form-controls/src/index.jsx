adcce55837284ae640473653cad98a04ecd70da9
Stefan Carrera <stefancarrera@gmail.com>
2021-07-09 19:38:03 -0500

    Completed react-form-controls.

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterFrom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange() {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit() {
+    event.preventDefault();
+    console.log('Submitted Email:', this.state.value);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Email:
+          <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterFrom />,
+  document.getElementById('root')
+);
