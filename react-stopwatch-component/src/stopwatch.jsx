05f42aae051b5ee0709098795d3c9cb7ed69d047
Stefan Carrera <stefancarrera@gmail.com>
2021-07-12 14:55:26 -0500

    Completed react-stopwatch-component.

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,27 +3,56 @@ import React from 'react';
 export default class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: false };
+    this.state = {
+      isToggleOn: false,
+      tick: 0
+    };
+
     this.handleClick = this.handleClick.bind(this);
+    this.iconSwitch = this.iconSwitch.bind(this);
+    this.faceClick = this.faceClick.bind(this);
   }
 
   handleClick() {
-    this.setState({ isToggleOn: !this.isToggleOn });
+    this.setState({ isToggleOn: !this.state.isToggleOn });
+    if (this.state.isToggleOn === false) {
+      let seconds = this.state.tick;
+      this.setState({
+        intervalId: setInterval(() => {
+          seconds++;
+          this.setState({ tick: seconds });
+        }, 1000)
+      });
+    } else {
+      this.setState({ intervalId: clearInterval(this.state.intervalId) });
+    }
   }
 
   iconSwitch() {
     if (this.state.isToggleOn === false) {
-      return 'far fa-play-circle';
-    } else {
-      return 'far fa-pause-circle';
+      return 'far fa-play-circle fa-3x';
+    } else if (this.state.isToggleOn === true) {
+      return 'far fa-pause-circle fa-3x';
+    }
+  }
+
+  faceClick() {
+    if (!this.state.isToggleOn) {
+      this.setState({ tick: 0 });
     }
   }
 
   render() {
     return (
       <div className="col">
-        <div>Time</div>
+        <div className="watchFace" onClick={this.faceClick}>
+          <p>
+          {this.state.tick}
+          </p>
+        </div>
+        <div className="btn">
         <i onClick={this.handleClick} className={this.iconSwitch()}></i>
+        </div>
       </div>
     );
   }
