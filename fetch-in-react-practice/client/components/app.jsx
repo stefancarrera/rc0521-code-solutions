331dc15a0b123136d73c9d3119dfe9d97963de39
Stefan Carrera <stefancarrera@gmail.com>
2021-07-14 18:33:03 -0500

    Completed fetch-in-react-practice.

---
+++ fetch-in-react-practice/client/components/app.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import PageTitle from './page-title';
+import TodoList from './todo-list';
+import TodoForm from './todo-form';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+  }
+
+  /**
+   * Use fetch to send a GET request to `/api/todos`.
+   * Then 😉, once the response JSON is received and parsed,
+   * update state with the received todos.
+   */
+  componentDidMount() {
+    fetch('/api/todos')
+      .then(response => response.json())
+      .then(data => this.setState({ todos: data }))
+      .catch(err => console.error('Fs in chat. Fetch Failed', err));
+  }
+
+  addTodo(newTodo) {
+    /**
+    * Use fetch to send a POST request to `/api/todos`.
+    * Then 😉, once the response JSON is received and parsed,
+    * add the created todo to the state array.
+    *
+    * Do not mutate the original state array, nor any objects within it.
+    * https://reactjs.org/docs/optimizing-performance.html#the-power-of-not-mutating-data
+    *
+    * TIP: Be sure to SERIALIZE the todo object in the body with JSON.stringify()
+    * and specify the "Content-Type" header as "application/json"
+    *
+    * TIP: Use Array.prototype.concat to create a new array containing the contents
+    * of the old array, plus the object returned by the server.
+    */
+
+    fetch('api/todos', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newTodo)
+    })
+      .then(response => response.json())
+      .then(data => this.setState(state => ({ todos: this.state.todos.concat(data) })))
+      .catch(err => console.error('Fs in chat. Post Failed', err));
+  }
+
+  toggleCompleted(todoId) {
+    const item = this.state.todos.find(element => element.todoId === todoId);
+    item.isCompleted = !item.isCompleted;
+    fetch(`api/todos/${todoId}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(item)
+    })
+      .then(response => response.json())
+      .then(item => {
+        const updatedTodos = this.state.todos;
+        updatedTodos[item.id] = item;
+        this.setState({ todos: updatedTodos });
+      })
+      .catch(err => console.error('Fs in chat. Patch Failed', err));
+  }
+  /**
+     * Find the index of the todo with the matching todoId in the state array.
+     * Get its "isCompleted" status.
+     * Make a new object containing the opposite "isCompleted" status.
+     * Use fetch to send a PATCH request to `/api/todos/${todoId}`
+     * Then 😉, once the response JSON is received and parsed,
+     * replace the old todo in the state array.
+     *
+     * NOTE: "toggle" means to flip back and forth, so clicking a todo
+     * in the list should "toggle" its isCompleted status back and forth.
+     *
+     * Do not mutate the original state array, nor any objects within it.
+     * https://reactjs.org/docs/optimizing-performance.html#the-power-of-not-mutating-data
+     *
+     * TIP: Be sure to SERIALIZE the updates in the body with JSON.stringify()
+     * And specify the "Content-Type" header as "application/json"
+     */
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col pt-5">
+            <PageTitle text="Todo App"/>
+            <TodoForm onSubmit={this.addTodo}/>
+            <TodoList todos={this.state.todos} toggleCompleted={this.toggleCompleted}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
